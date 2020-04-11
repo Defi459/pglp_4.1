c877f656cc1ea4dbf5b82f5e0ff131ba3985d268
Edouard Longueville <edouard.longueville@ens.uvsq.fr>
2020-04-11 22:44:14 +0200

    Test unitaire

--- src/test/java/uvsq/PersonnelTest.java
+++ src/test/java/uvsq/PersonnelTest.java
@@ -1,5 +1,6 @@
 package uvsq;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,12 +10,17 @@ import org.junit.Test;
  */
 public class PersonnelTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void PersonnelTest()
     {
-        assertTrue( true );
+        Personnel.Builder b;
+        Personnel p;
+        b = new Personnel.Builder("Longueville", "Edouard","Etudiant");
+        b.addphone("0000");
+        p = b.build();
+        assertEquals("Longueville",p.getNom());
+        assertEquals("Edouard",p.getPrenom());
+        assertEquals("Etudiant",p.getFonction());
     }
 }
